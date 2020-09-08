614f0683092213dfc73c8a4ef55533cf515b63cc
Sonu <sonu3660@gmail.com>
2020-09-08 18:21:15 +0530

    added nodemailer and captcha

--- config/captcha.js
+++ config/captcha.js
@@ -1,30 +1,30 @@
-const request = require('request');
+const request = require("request");
 
-module.exports.isCaptchaVerified=async(req)=>{
-    if(
-        req.body['g-recaptcha-response'] === undefined || 
-        req.body['g-recaptcha-response'] === '' || 
-        req.body['g-recaptcha-response'] === null
-    ){
-        req.flash(
-            'error',
-            'Invalid Captcha!'
-        );
-        return false;
-    }
+module.exports.isCaptchaVerified = async (req) => {
+   if (
+      req.body["g-recaptcha-response"] === undefined ||
+      req.body["g-recaptcha-response"] === "" ||
+      req.body["g-recaptcha-response"] === null
+   ) {
+      req.flash("error", "Invalid Captcha!");
+      return false;
+   }
 
-    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + process.env.CAPTCHA_SECRET_KEY + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-    
-    await request(verificationURL,function(error,response,body) {
-        body = JSON.parse(body);
-    
-        if(body.success !== undefined && !body.success) {
-            req.flash(
-                'error',
-                'Captcha Verifiction Failed!'
-            );
-            return false;
-        }
-    });
-    return true;
-}
+   const verificationURL =
+      "https://www.google.com/recaptcha/api/siteverify?secret=" +
+      process.env.CAPTCHA_SECRET_KEY +
+      "&response=" +
+      req.body["g-recaptcha-response"] +
+      "&remoteip=" +
+      req.connection.remoteAddress;
+
+   await request(verificationURL, function (error, response, body) {
+      body = JSON.parse(body);
+
+      if (body.success !== undefined && !body.success) {
+         req.flash("error", "Captcha Verifiction Failed!");
+         return false;
+      }
+   });
+   return true;
+};
--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,13 +1,12 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-module.exports.transporter=nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    post: 587,
-    //secure: false,
-    auth: {
-        user: process.env.gmailId ,
-        pass: process.env.gmailPass
-    },
+module.exports.transporter = nodemailer.createTransport({
+   service: "gmail",
+   host: "smtp.gmail.com",
+   post: 587,
+   auth: {
+      user: process.env.gmailId,
+      pass: process.env.gmailPass,
+   },
 });
